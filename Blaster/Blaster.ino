263687526739d891a0652f1e95f330be19c39848
Alex Jasper <aj39aj10@gmail.com>
2019-11-02 15:19:17 -0400

    Update Blaster with hit timout and server connection

--- Blaster/Blaster.ino
+++ Blaster/Blaster.ino
@@ -1,12 +1,17 @@
 #include <WiFiNINA.h>
 
+#define IRLED 0
+#define HITLED 0
+#define BOARDLED LED_BUILTIN
+#define HIT 'h'
+
 char ssid[] = "Blue1";          //  your network SSID (name)
 char pw[] = "BlueTeam1";        // your network password
 
+IPAddress server(192,168,1,1);
 int port = 80;
 
 int status = WL_IDLE_STATUS;
-//char servername[]="google.com";  // remote server we will connect to
 
 WiFiClient client;
 
@@ -15,9 +20,13 @@ IPAddress ip;
 void setup() 
 {
   Serial.begin(9600);
-  delay(1000);
-  Serial.print("Attempting to connect to: ");
-  Serial.println(ssid);
+  pinMode(BOARDLED, OUTPUT);
+  digitalWrite(BOARDLED, LOW);
+
+  //REMOVE AFTER DEBUGGING!!!
+  while(!Serial);
+  
+  Serial.println("Attempting to connect to: " + String(ssid));
 
   status = WiFi.begin(ssid, pw);
   while( status != WL_CONNECTED) 
@@ -27,11 +36,31 @@ void setup()
      status = WiFi.begin(ssid, pw);
   }
   Serial.println("Connected to wifi");
+  digitalWrite(BOARDLED, HIGH);
   ip = WiFi.localIP();
   Serial.println(ip);
+
+  if(client.connect(server, port))
+  {
+    Serial.println("Connected to server");
+  }
 }
 
 void loop() 
 {
-
+  //Timeout
+  if(client.available())
+  {
+    char c = client.read();
+    if(c == HIT)
+    {
+      Serial.println("HIT");
+      for(int i = 0; i < 20; i++)
+      {
+        digitalWrite(BOARDLED, !digitalRead(BOARDLED));
+        delay(500);
+      }
+    }
+  }
+  Serial.println("Doing other tasks");
 }
